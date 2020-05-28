b8452f4502dd2e279bbc82804f8d1d628ac8338b
akimovaen <akimova.lena.n@gmail.com>
2020-05-28 12:49:18 +0300

    create models and fill the database

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Menu, Items
+
+
 # Register your models here.
+
+admin.site.register(Menu)
+admin.site.register(Items)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Items(models.Model):
+    TRAITS = (
+        ('S', 'Small'),
+        ('L', 'Large'),
+        ('A', 'Addition')
+    )
+    
+    name = models.CharField(max_length=30)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name="menu")
+    trait = models.CharField(max_length=1, choices=TRAITS, null=True, blank=True)
+    price = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.menu} - {self.name} ({self.trait})"
